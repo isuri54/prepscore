020f9ba63cde13bdda5cf8a8a2129ebb6776dd3d
isuri54 <154318794+isuri54@users.noreply.github.com>
2025-12-17 00:56:16 +0530

    Update backend

--- backend/main.py
+++ backend/main.py
@@ -20,17 +20,11 @@ model = joblib.load("interview_model.pkl")
 scaler = joblib.load("scaler.pkl")
 mlb = joblib.load("skills_encoder.pkl")
 
-with open("industry_columns.pkl", "rb") as f:
-    industry_columns = pickle.load(f) 
-
-
 class InputData(BaseModel):
     Skills: list[str]
     GPA: float
     Experience: float
     Certifications: float
-    Age: float
-    Industry: str
 
 
 @app.post("/predict")
@@ -40,18 +34,10 @@ def predict_score(data: InputData):
     skills_encoded = mlb.transform([data.Skills])[0]
 
     # Scale numeric values
-    numeric_scaled = scaler.transform([[data.GPA, data.Experience, data.Certifications, data.Age]])[0]
-
-    # One-hot encode Industry
-    industry_vector = [0] * len(industry_columns)
-    industry_key = f"Industry_{data.Industry}"
-
-    if industry_key in industry_columns:
-        idx = industry_columns.index(industry_key)
-        industry_vector[idx] = 1
+    numeric_scaled = scaler.transform([[data.GPA, data.Experience, data.Certifications]])[0]
 
     # Combine final feature vector
-    final_features = np.concatenate([skills_encoded, numeric_scaled, industry_vector])
+    final_features = np.concatenate([skills_encoded, numeric_scaled])
 
     # Predict
     prediction = model.predict([final_features])[0]
