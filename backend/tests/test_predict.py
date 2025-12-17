83ee255e2ca425988b9f3fbe09723927210db97b
isuri54 <154318794+isuri54@users.noreply.github.com>
2025-12-17 23:19:07 +0530

    Create unit testing

---
+++ backend/tests/test_predict.py
@@ -0,0 +1,17 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_predict_success():
+    payload = {
+        "Skills": ["Python", "Java"],
+        "GPA": 3.4,
+        "Experience": 2,
+        "Certifications": 1
+    }
+
+    response = client.post("/predict", json=payload)
+
+    assert response.status_code == 200
+    assert "Predicted_Score" in response.json()
